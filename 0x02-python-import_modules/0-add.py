163a0646b041bc3a0b11d5eff3fa8e056c741fc7
Onkababes <onkamokgwasa@gmail.com>
2023-11-30 15:09:49 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
 from add_0 import add
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = 3")
+
+    answer = add(a, b)
+
+    print(f"{a} + {b} = {answer}")
